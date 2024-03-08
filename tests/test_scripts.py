8e09b719d48701166b12257cf2a39c935bd22712
Florian Ritterhoff <florian.ritterhoff@hm.edu>
2024-03-08 20:10:28 +0100

    tests: start testing of scripts

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -5,42 +5,42 @@ import unittest
 from importlib.machinery import SourceFileLoader
 from importlib.util import spec_from_loader, module_from_spec
 
-SCRIPTS = [
-    'creategoogleauthenticator-file',
-    'getgooglecodes',
-#    '-convert-base32.py',
-    'edumfa-create-ad-users',
-    'edumfa-create-certificate',
-    'edumfa-create-pwidresolver-user',
-    'edumfa-create-sqlidresolver-user',
-    'edumfa-cron',
-    'edumfa-expired-users',
-    'edumfa-export-linotp-counter.py',
-    'edumfa-export-edumfa-counter.py',
-    'edumfa-fix-access-rights',
-    'edumfa-get-serial',
-    'edumfa-get-unused-tokens',
-    'edumfa-migrate-linotp.py',
-    'edumfa-pip-update',
-    'edumfa-queue-huey',
-    'edumfa-standalone',
-    'edumfa-sync-owncloud.py',
-    'edumfa-token-janitor',
-    'edumfa-update-counter.py',
-    'edumfa-update-linotp-counter.py',
-    'edumfa-user-action',
-    'edumfa-usercache-cleanup',
-    'ssha.py',
-    '../edumfa-manage'
-]
+from edumfa.commands.manage.main import cli as edumfa_manage
+from edumfa.app import create_app
+from edumfa.models import (db)
 
 
 class ScriptsTestCase(unittest.TestCase):
+    app = None
+    app_context = None
 
-    def test_01_loading_scripts(self):
-        for script in SCRIPTS:
-            with self.subTest(script=script):
-                loader = SourceFileLoader(script, os.path.join('tools', script))
-                spec = spec_from_loader(loader.name, loader)
-                mod = module_from_spec(spec)
-                loader.exec_module(mod)
+    @classmethod
+    def setUpClass(cls):
+        cls.app = create_app(config_name="testing", config_file="", silent=True)
+        cls.app_context = cls.app.app_context()
+        cls.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.commit()
+        db.session.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        db.drop_all()
+        db.engine.dispose()
+        cls.app_context.pop()
+
+    def test_01_edumfa_admin(self):
+        runner = self.app.test_cli_runner()
+        result = runner.invoke(edumfa_manage, ["admin", "add", "Admin-User"], input="Test\nTest")
+        assert result.exit_code == 0
+        assert "was registered successfully" in result.output
+        result = runner.invoke(edumfa_manage, ["admin", "list"])
+        assert result.exit_code == 0
+        assert "Admin-User" in result.output
+        result = runner.invoke(edumfa_manage, ["admin", "delete", "Admin-User"])
+        assert result.exit_code == 0
+        result = runner.invoke(edumfa_manage, ["admin", "list"])
+        assert result.exit_code == 0
+        assert "Admin-User" not in result.output
