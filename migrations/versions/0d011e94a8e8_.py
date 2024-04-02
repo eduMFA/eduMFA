af7f467a16243460be7ab0c18a6b2dcc3b3416a6
Florian Ritterhoff <32478819+fritterhoff@users.noreply.github.com>
2024-04-02 07:37:56 +0200

    feat: more detailed logging for eduMFA migration script (#44)

--- migrations/versions/0d011e94a8e8_.py
+++ migrations/versions/0d011e94a8e8_.py
@@ -1,4 +1,4 @@
-"""empty message
+"""Migrate from privacyIDEA to eduMFA
 
 Revision ID: 0d011e94a8e8
 Revises: 5cb310101a1f
@@ -22,13 +22,18 @@ def dialect_supports_sequences():
 
 
 def upgrade():
+
+    print("Migrating from privacyIDEA to eduMFA")
     try:
+        print(" * Renaming column privacyidea_server to edumfa_server")
         op.alter_column("pidea_audit", "privacyidea_server", new_column_name="edumfa_server", existing_type=sa.String(length=255))
+        print(" * Renaming table pidea_audit to mfa_audit")
         op.rename_table("pidea_audit", "mfa_audit")
     except Exception as e:
         print("Renaming table pidea_audit to mfa_audit failed")
         print(e)
     try:
+        print(" * Renaming table privacyideaserver to edumfaserver")
         op.rename_table("privacyideaserver", "edumfaserver")
     except Exception as e:
         print("Renaming table privacyideaserver to edumfaserver failed")
@@ -37,14 +42,17 @@ def upgrade():
     try:
         if dialect_supports_sequences():
             if context.get_context().dialect.name in ['mariadb', 'mysql']:
+                print(" * Renaming table privacyideaserver_seq to edumfaserver_seq")
                 op.rename_table("privacyideaserver_seq", "edumfaserver_seq")
             else:
+                print(" * Renaming sequence privacyideaserver_id_seq to edumfaserver_id_seq")
                 op.execute("ALTER SEQUENCE privacyideaserver_id_seq RENAME TO edumfaserver_id_seq")
     except Exception as e:
         print("Renaming sequence for privacyideaserver to edumfaserver failed")
         print(e)
 
     try:
+        print(" * Renaming column pinode to edumfanode")
         op.alter_column("policy", "pinode", new_column_name="edumfanode", existing_type=sa.String(length=256))
     except Exception as e:
         print("Migrating pinode to edumfanode failed")
@@ -53,7 +61,9 @@ def upgrade():
     bind = op.get_bind()
     session = orm.Session(bind=bind)
     try:
+        print(" * Replacing login_mode=privacyIDEA with login_mode=eduMFA")
         for row in session.query(Policy).filter(Policy.action.like("login_mode=privacyIDEA")):
+            print(f' ** Replacing login_mode in policy {row.name} (id: {row.id}) with existing action {row.action}')
             row.action = row.action.replace("login_mode=privacyIDEA", "login_mode=eduMFA")
         session.commit()
     except Exception as e:
@@ -63,9 +73,12 @@ def upgrade():
 
     session = orm.Session(bind=bind)
     try:
+        print(" * Replacing privacyideaserver_read and privacyideaserver_write policies ")
         for row in session.query(Policy).filter(Policy.action.like("privacyideaserver_read")):
+            print(f' ** Replacing policy for {row.name} ({row.id}) with action {row.action}')
             row.action = row.action.replace("privacyideaserver_read", "edumfaserver_read")
         for row in session.query(Policy).filter(Policy.action.like("privacyideaserver_write")):
+            print(f' ** Replacing policy for {row.name} ({row.id}) with action {row.action}')
             row.action = row.action.replace("privacyideaserver_write", "edumfaserver_write")
         session.commit()
     except Exception as e:
@@ -75,7 +88,9 @@ def upgrade():
 
     session = orm.Session(bind=bind)
     try:
+        print(" * Replacing providernames for smsgateways")
         for row in session.query(SMSGateway).filter(SMSGateway.providermodule.like("privacyidea.lib.%")):
+            print(f' ** Replacing providername for {row.identifier} ({row.id}) with provider {row.providermodule}')
             row.providermodule = row.providermodule.replace("privacyidea.lib.", "edumfa.lib.")
         session.commit()
     except Exception as e:
