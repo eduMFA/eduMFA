d709293115a223c12024c764b0afb1ae27521e80
Lucas Hardt <lucas.hardt@fu-berlin.de>
2024-04-26 10:58:42 +0200

    Add logo for dark theme

--- doc/conf.py
+++ doc/conf.py
@@ -125,7 +125,10 @@ html_theme = 'furo'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "light_logo": "images/edumfa-logo.png",
+    "dark_logo": "images/edumfa-logo-dark.png",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = ['_themes/flask-sphinx-themes']
@@ -140,12 +143,12 @@ html_theme = 'furo'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "images/eduMFA-logo.png"
+html_logo = "images/edumfa-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "images/eduMFA-icon.png"
+html_favicon = "images/edumfa-icon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
