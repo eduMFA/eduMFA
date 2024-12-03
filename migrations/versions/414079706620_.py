90cb85b0326c0b279f4e39ba6fbe007a3cb5051c
Florian Ritterhoff <32478819+fritterhoff@users.noreply.github.com>
2024-12-03 16:05:03 +0100

    fix: increase column sizes (#461)

--- migrations/versions/414079706620_.py
+++ migrations/versions/414079706620_.py
@@ -12,27 +12,30 @@ down_revision = '7d82d8b680a9'
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import BigInteger
+from sqlalchemy.dialects import postgresql, mysql, sqlite
 
+BigIntegerType = BigInteger().with_variant(postgresql.BIGINT(), 'postgresql').with_variant(mysql.BIGINT(), 'mysql').with_variant(sqlite.INTEGER(), 'sqlite')
 
 def upgrade():
     op.alter_column('mfa_audit', 'id',
                    existing_type=sa.Integer(),
-                   type_=sa.BigInteger(),
+                   type_=BigIntegerType,
                    existing_nullable=False)
     op.alter_column('challenge', 'id',
                    existing_type=sa.Integer(),
-                   type_=sa.BigInteger(),
+                   type_=BigIntegerType,
                    existing_nullable=False)
     pass
 
 
 def downgrade():
     op.alter_column('mfa_audit', 'id',
-                   existing_type=sa.BigInteger(),
+                   existing_type=BigIntegerType,
                    type_=sa.Integer(),
                    existing_nullable=False)
     op.alter_column('challenge', 'id',
-                   existing_type=sa.BigInteger(),
+                   existing_type=BigIntegerType,
                    type_=sa.Integer(),
                    existing_nullable=False)
     pass
