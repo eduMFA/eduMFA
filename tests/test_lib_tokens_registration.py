ad3cc678e56a4cad146c8cf58bd3172ca424cedc
Paul Mainz <paul.mainz@fu-berlin.de>
2025-01-20 14:58:27 +0100

    Bugfix in test

--- tests/test_lib_tokens_registration.py
+++ tests/test_lib_tokens_registration.py
@@ -49,7 +49,6 @@ class RegistrationTokenTestCase(MyTestCase):
         registrationcode = init_detail.get("registrationcode")
         # the registrationcode should now equal the otpkey-param
         self.assertEqual("test_registration_key", registrationcode)
-        self.assertTrue(int(registrationcode))
 
     def test_02_class_methods(self):
         db_token = Token.query.filter(Token.serial == self.serial1).first()
