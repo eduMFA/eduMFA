b8e6255ffe01c12e5caba07afe4430116cceeca5
Florian Ritterhoff <florian.ritterhoff@hm.edu>
2025-08-23 15:17:21 +0200

    feat: generate more output for webauthn migration

--- migrations/versions/1b2262ddbf6b_.py
+++ migrations/versions/1b2262ddbf6b_.py
@@ -1,6 +1,6 @@
-"""Decrypt WebAuthn tokens. 
+"""Decrypt WebAuthn tokens.
 
-Before this version, WebAuthn tokens were stored in an encrypted format. This migration will decrypt the tokens and store them in a plain format. 
+Before this version, WebAuthn tokens were stored in an encrypted format. This migration will decrypt the tokens and store them in a plain format.
 Decrypting the credential id is not really necessary, as it is not sensitive information.
 
 Revision ID: 1b2262ddbf6b
@@ -10,11 +10,10 @@ Create Date: 2025-08-23 08:05:06.963794
 """
 
 # revision identifiers, used by Alembic.
-revision = '1b2262ddbf6b'
-down_revision = 'a844dc0c3fb9'
+revision = "1b2262ddbf6b"
+down_revision = "a844dc0c3fb9"
 
 from alembic import op
-import sqlalchemy as sa
 from sqlalchemy import orm
 
 from edumfa.models import Token
@@ -24,7 +23,13 @@ def upgrade():
     print("Migrating WebAuthn tokens credential_id to an unencrypted format")
     bind = op.get_bind()
     session = orm.Session(bind=bind)
-    for token in session.query(Token).filter(Token.tokentype == 'webauthn').all():
+    tokens = session.query(Token).filter(Token.tokentype == "webauthn").all()
+    total = len(tokens)
+    if total == 0:
+        print(" -> Nothing to decrypt!")
+        return
+    for idx, token in enumerate(tokens, start=1):
+        print(" -> Decrypting credential id for WebAuthN token {} ({}/{})".format(token.id, idx, total))
         token.set_otpkey(token.get_otpkey().getKey(), encrypted=False)
         session.add(token)
     session.commit()
@@ -34,7 +39,13 @@ def downgrade():
     print("Migrating WebAuthn tokens credential_id to an encrypted format")
     bind = op.get_bind()
     session = orm.Session(bind=bind)
-    for token in session.query(Token).filter(Token.tokentype == 'webauthn').all():
+    tokens = session.query(Token).filter(Token.tokentype == "webauthn").all()
+    total = len(tokens)
+    if total == 0:
+        print(" -> Nothing to encrypt!")
+        return
+    for idx, token in enumerate(tokens, start=1):
+        print(" -> Encrypting credential id for WebAuthN token {} ({}/{})".format(token.id, idx, total))
         token.set_otpkey(token.get_otpkey(encrypted=False).getKey())
         session.add(token)
     session.commit()
