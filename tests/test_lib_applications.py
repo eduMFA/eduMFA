be7ba1fbade03bac41eea83538d9b1c90bf9e025
Lucas Hardt <lucas.hardt@fu-berlin.de>
2025-10-13 15:20:17 +0200

    Fix tests due to incorrect assertion

--- tests/test_lib_applications.py
+++ tests/test_lib_applications.py
@@ -76,7 +76,7 @@ class SSHApplicationTestCase(MyTestCase):
             self.assertFalse(auth_item)
             mock_log.assert_called_with(
                 "The requested user Idefix does "
-                "not match the user option (Idefix) of the SSH application."
+                "not match the user option (None) of the SSH application."
             )
 
     def test_03_get_auth_item_unsupported(self):
