e4ab4bbf81894a5dedcdf336f5893c1e0b252549
aleyna72072 <aleyna.nashertaher@daasi.de>
2025-10-15 13:29:39 +0200

    Multiple improvements to the docker image (#779)
    Co-authored-by: Lucas Hardt <lucas.hardt@fu-berlin.de>

--- deploy/docker-setup.sh
+++ deploy/docker-setup.sh
@@ -1,42 +1,58 @@
-#!/bin/bash
+#!/usr/bin/bash
 set -e
 
-# MariaDB needs a second
-sleep 5
+GEN_PWD="$(openssl rand -base64 42)"
+EDUMFA_ADMIN_USER="${EDUMFA_ADMIN_USER:-admin}"
+EDUMFA_ADMIN_PASS="${EDUMFA_ADMIN_PASS:-$GEN_PWD}"
 
 # Create enckey if doesn't exist yet
-if [ ! -f /etc/edumfa/enckey ]; then
-  edumfa-manage create_enckey
-fi
+edumfa-manage -q create_enckey || true
 
 # Create audit keys if they don't exist yet
-if [[ ! -f /etc/edumfa/pubkey.pem && ! -f /etc/edumfa/private.pem ]]; then
-  edumfa-manage create_audit_keys
-fi
+edumfa-manage -q create_audit_keys || true
 
 # Create DB
 echo "Creating DB"
-edumfa-manage createdb
+# FIXME: this creates a exception trace on every attempt
+attempts=10
+until edumfa-manage -q create_tables
+do
+  if [[ $attempts -eq 0 ]]; then
+    echo "Exhausted database connection tries. Stopping."
+    exit 1
+  else
+    echo "Cannot connect to database. Trying again..."
+    sleep 3
+    attempts=$((attempts-1))
+  fi
+done
 
 # Check and stamp DB
-STAMP=$(edumfa-manage db current -d /usr/local/lib/edumfa/migrations 2>/dev/null)
+STAMP=$(edumfa-manage -q db current -d /usr/local/lib/edumfa/migrations 2>/dev/null)
 if [[ -z "${STAMP//Running online/}" ]]; then
-  edumfa-manage db stamp head -d /usr/local/lib/edumfa/migrations
+  edumfa-manage -q db stamp head -d /usr/local/lib/edumfa/migrations
 fi
 
 # Upgrading DB
 echo "Upgrading Database"
-edumfa-manage db upgrade -d /usr/local/lib/edumfa/migrations
+edumfa-schema-upgrade /usr/local/lib/edumfa/migrations --skipstamp
 
 # Create admin user
 echo "Creating Admin"
-edumfa-manage admin add $EDUMFA_ADMIN_USER -p $EDUMFA_ADMIN_PASS
+edumfa-manage -q admin add "$EDUMFA_ADMIN_USER" -p "$EDUMFA_ADMIN_PASS"
 
 # Execute user scripts
-if compgen -G "/opt/edumfa/user-scripts/*.sh" > /dev/null; then
-    echo "Executing User-Scripts"
-    bash /opt/edumfa/user-scripts/*.sh
-fi
+shopt -s nullglob
+for script in /opt/edumfa/user-scripts/*.sh; do
+  echo "Executing user script $script..."
+  bash $script
+done
+
+echo "
+    You can login with the following credentials:
+    username: $EDUMFA_ADMIN_USER
+    password: $EDUMFA_ADMIN_PASS
+"
 
 echo "Starting Server"
 gunicorn --bind 0.0.0.0:8000 --workers 4 app
