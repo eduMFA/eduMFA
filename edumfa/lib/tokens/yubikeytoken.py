a0bd652fb03f4a43333032a6452a6bdde60d1f62
Lucas Hardt <lucas.hardt@fu-berlin.de>
2026-01-21 13:35:29 +0100

    revert changes

--- edumfa/lib/tokens/yubikeytoken.py
+++ edumfa/lib/tokens/yubikeytoken.py
@@ -421,12 +421,12 @@ class YubikeyTokenClass(TokenClass):
                     data["status"] = "BAD_OTP"
 
             data["h"] = yubico_api_signature(data, api_key)
-        response = f"""nonce={nonce}
+        response = """nonce={nonce}
 otp={otp}
 status={status}
 timestamp={timestamp}
-h={data["h"]}
-"""
+h={h}
+""".format(**data)
 
         return "plain", response
 
