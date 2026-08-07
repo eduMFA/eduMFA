399ea356ef217f5c0f1ada14c66d362648e27a9a
Angela Schlieben <angela.schlieben@gmail.com>
2026-08-07 13:54:06 +0200

    3) refactor(monitoring): switch to deferred pattern for requests

--- edumfa/static/components/monitoring/factories/monitoring.js
+++ edumfa/static/components/monitoring/factories/monitoring.js
@@ -1,8 +1,11 @@
-myApp.factory("MonitoringFactory", ["AuthFactory", "$http", "monitoringUrl",
-    function (AuthFactory, $http, monitoringUrl) {
+myApp.factory("MonitoringFactory", ["AuthFactory", "$http", "monitoringUrl", "$q",
+    function (AuthFactory, $http, monitoringUrl, $q) {
         /**
          Each service - just like this service factory - is a singleton.
          */
+
+        var cancelers = {};
+
         return {
 
             get_stats_keys: function (callback) {
@@ -12,13 +15,28 @@ myApp.factory("MonitoringFactory", ["AuthFactory", "$http", "monitoringUrl",
                     function (error) { AuthFactory.authError(error.data) });
             },
 
-            get_monitored: function (stats_key, params, callback) {
+            get_monitored: function (stats_key, params, cacheKey, callback) {
+                if (cancelers[cacheKey]) {
+                    cancelers[cacheKey].resolve();
+                }
+                cancelers[cacheKey] = $q.defer();
+
                 $http.get(monitoringUrl + "/" + stats_key, {
                     headers: { 'Authorization': AuthFactory.getAuthToken() },
                     params: params,
-                }).then(function (response) { callback(response.data) },
+                    timeout: cancelers[cacheKey].promise,
+                }).then(function (response) {
+                    delete cancelers[cacheKey];
+                    callback(response.data)
+                },
                     function (error) { AuthFactory.authError(error.data) });
             },
+            cancelAll: function () {
+                Object.keys(cancelers).forEach(function (key) {
+                    cancelers[key].resolve();
+                });
+                cancelers = {};
+            }
         }
     }]);
 
@@ -28,8 +46,39 @@ myApp.directive('timelineChart', function () {
         scope: {
             tokenTimeline: '=',
         },
-        template: `<div style="position:relative; width:100%; height:300px;"><canvas></canvas></div>`,
+        template: `
+            <div class="chartOuter" style="width:100%; overflow-x:auto; overflow-y:hidden; position:relative;">
+                <div class="chartWrapper" style="position:relative; height:400px;">
+                    <canvas class="chartCanvas"></canvas>
+                </div>
+            </div>
+        `,
         link: function (scope, element) {
+            var root = element[0];
+            var outer = root.querySelector('.chartOuter');
+            var wrapper = root.querySelector('.chartWrapper');
+            var canvas = root.querySelector('canvas');
+            function setWidth(datasets) {
+                var maxPoints = 0;
+                (datasets || []).forEach(function (ds) {
+                    if (ds.data && ds.data.length > maxPoints) maxPoints = ds.data.length;
+                });
+
+                var pxPerPoint = 15;
+                var outerWidth = outer.clientWidth;
+                var neededWidth = maxPoints * pxPerPoint;
+                var dpr = window.devicePixelRatio || 1;
+                var browserMax = 30000;
+                var maxAllowedWidth = Math.floor(browserMax / dpr);
+                var width = Math.max(outerWidth, Math.min(neededWidth, maxAllowedWidth));
+
+                wrapper.style.width = width + 'px';
+                canvas.style.width = width + 'px';
+                canvas.style.height = '400px';
+
+                chart.resize();
+                chart.update();
+            }
             var chart = new Chart(element.find('canvas')[0], {
                 type: 'line',
                 data: {
@@ -38,6 +87,7 @@ myApp.directive('timelineChart', function () {
                 options: {
                     animation: false,
                     parsing: false,
+                    normalized: true,
                     responsive: true,
                     maintainAspectRatio: false,
                     plugins: {
@@ -74,7 +124,7 @@ myApp.directive('timelineChart', function () {
             scope.$watch('tokenTimeline', function (c) {
                 if (!c) return;
                 chart.data.datasets = c.data.datasets;
-                chart.update();
+                setWidth(c.data.datasets);
             }, true);
             scope.$on('$destroy', function () { chart.destroy(); });
         }
